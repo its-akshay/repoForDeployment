150e90f79fdabab9385e06cf9c0a13195d091050
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-10-08 19:15:51 +0530

    final

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, Markup
+import pandas as pd
+import numpy as np
+import sklearn
+import os
+import pickle
+import warnings
+from utils.fertilizer import fertilizer_dic
+
+
+app = Flask(__name__)
+
+loaded_model = pickle.load(open("models/model.pkl", 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
+
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Harvestify - Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
+
+
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    N = int(request.form['Nitrogen'])
+    P = int(request.form['Phosporus'])
+    K = int(request.form['Potassium'])
+    temp = float(request.form['Temperature'])
+    humidity = float(request.form['Humidity'])
+    ph = float(request.form['pH'])
+    rainfall = float(request.form['Rainfall'])
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+
+    prediction = loaded_model.predict(single_pred)
+
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+    if prediction[0] in crop_dict:
+        crop = crop_dict[prediction[0]]
+        # result = "{} is the best crop to be cultivated right there".format(crop)
+        result = "{}".format(crop)
+
+    else:
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+
+    return render_template('home.html', prediction=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
